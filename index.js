ce9c996ee6a1979b3cc6b5dcf296d1bd93042970
Ron Sharma <sharma.ro@husky.neu.edu>
2021-12-09 20:18:25 -0500

    add Memcache

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ require("dotenv").config();
 
 const port = process.env.PORT || 4000;
 
-cron.schedule("17 18 * * *", async () => {
+cron.schedule("20 20 * * *", async () => {
   const instagramLoginFunction = async () => {
     // Persist cookies after Instagram client log in
     const cookieStore = new FileCookieStore("./cookies.json");
