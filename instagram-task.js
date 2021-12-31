3d74d7546335624fd5928359e474459eaf77e756
Ron Sharma <sharma.ro@husky.neu.edu>
2021-12-31 16:02:43 -0500

    Find issue with async

--- instagram-task.js
+++ instagram-task.js
@@ -2,6 +2,11 @@ const Instagram = require("instagram-web-api");
 const FileCookieStore = require("tough-cookie-filestore2");
 const memjs = require('memjs');
 
+const memCacheClient = memjs.Client.create(process.env.MEMCACHEDCLOUD_SERVERS, {
+    username: process.env.MEMCACHEDCLOUD_USERNAME,
+    password: process.env.MEMCACHEDCLOUD_PASSWORD
+  });
+
 require("dotenv").config();
 
 async function instagramLoginFunction() {
@@ -30,11 +35,7 @@ async function instagramLoginFunction() {
       const res = await client.getUserByUsername({username: process.env.INSTAGRAM_USERNAME})
 
       console.log(`${new Date()} : Setting Instagram Profile In Memory for the Day`)
-
-      memjs.Client.create(process.env.MEMCACHEDCLOUD_SERVERS, {
-        username: process.env.MEMCACHEDCLOUD_USERNAME,
-        password: process.env.MEMCACHEDCLOUD_PASSWORD
-      }).set("instagramData", JSON.stringify(res))
+      await memCacheClient.set("instagramData", JSON.stringify(res))
 
     } catch (err) {
       console.log(`${new Date()} : ${err.message}`);
