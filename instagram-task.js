305ff30303df4518bbf5ef3018ded0b03ee16b9d
Ron Sharma <sharma.ro@husky.neu.edu>
2021-12-31 19:09:52 -0500

    Add memcache quit method

--- instagram-task.js
+++ instagram-task.js
@@ -2,16 +2,12 @@ const Instagram = require("instagram-web-api");
 const FileCookieStore = require("tough-cookie-filestore2");
 const memjs = require('memjs');
 
+require("dotenv").config();
+
 const memCacheClient = memjs.Client.create(process.env.MEMCACHEDCLOUD_SERVERS, {
     username: process.env.MEMCACHEDCLOUD_USERNAME,
     password: process.env.MEMCACHEDCLOUD_PASSWORD
-  }, {
-    failover: true,  // default: false
-    timeout: 1,      // default: 0.5 (seconds)
-    keepAlive: true  // default: false
   });
-
-require("dotenv").config();
 
 async function instagramLoginFunction() {
     // Persist cookies after Instagram client log in
@@ -36,20 +32,15 @@ async function instagramLoginFunction() {
 
       console.log(`${new Date()} : Login successful!`);
 
-      const res = await client.getUserByUsername({username: process.env.INSTAGRAM_USERNAME})
-
-      return res
+      client.getUserByUsername({username: process.env.INSTAGRAM_USERNAME}).then((res) => {
+        console.log(`${new Date()} : Setting Instagram Profile In Memory for the Day`)
+        memCacheClient.set("instagramData", JSON.stringify(res)).then((res) => {
+            memCacheClient.quit()
+        });
+      })
     } catch (err) {
       console.log(`${new Date()} : ${err.message}`);
     }
 }
 
-instagramLoginFunction().then((res) => {
-    console.log(`${new Date()} : Setting Instagram Profile In Memory for the Day`)
-    memCacheClient.set("instagramData", JSON.stringify(res)).then((res) => {
-        console.log(res)
-        console.log('Done')
-        return
-    })
-    
-})
+instagramLoginFunction();
